cf8746cfeab9c228ce383003d5a61603abec1a85
Prkizir <A01020382@itesm.mx>
2019-11-28 19:04:14 -0600

    huehue

--- server/exec/cu/filter.cu
+++ server/exec/cu/filter.cu
@@ -9,6 +9,32 @@
 
 typedef enum color {BLUE, GREEN, RED} Color;
 
+__global__ void blur(unsigned char *src, unsigned char *dest, int width, int heigth, int blur_window, int step, int channels) {
+	int i, j, side_pixels, cells;
+	int ren, col, tmp_ren, tmp_col;
+	float r, g, b;
+
+	ren = blockIdx.x;
+	col = threadIdx.x;
+	side_pixels = (blur_window - 1) / 2;
+	cells = (blur_window * blur_window);
+	r = 0; g = 0; b = 0;
+	for (i = -side_pixels; i <= side_pixels; i++) {
+		for (j = -side_pixels; j <= side_pixels; j++) {
+			tmp_ren = MIN( MAX(ren + i, 0), heigth - 1 );
+			tmp_col = MIN( MAX(col + j, 0), width - 1);
+
+			r += (float) src[(tmp_ren * step) + (tmp_col * channels) + RED];
+			g += (float) src[(tmp_ren * step) + (tmp_col * channels) + GREEN];
+			b += (float) src[(tmp_ren * step) + (tmp_col * channels) + BLUE];
+		}
+	}
+
+	dest[(ren * step) + (col * channels) + RED] =  (unsigned char) (r / cells);
+	dest[(ren * step) + (col * channels) + GREEN] = (unsigned char) (g / cells);
+	dest[(ren * step) + (col * channels) + BLUE] = (unsigned char) (b / cells);
+}
+
 __global__ void gray(unsigned char *src, unsigned char *dest, int width, int height, int step, int channels){
   int ren, col;
 	float r, g, b, avg;
@@ -28,6 +54,10 @@ __global__ void gray(unsigned char *src, unsigned char *dest, int width, int hei
 	dest[(ren * step) + (col * channels) + BLUE] = (unsigned char) (avg);
 }
 
+__global__ void edge(unsigned char *src, unsigned char *dest, int width, int height, int step, int channels){
+
+}
+
 int main(int argc, char* argv[]) {
 	int step, size;
 	unsigned char *dev_src, *dev_dest;
@@ -35,6 +65,7 @@ int main(int argc, char* argv[]) {
 	char src_name[255];
 	char filter_t[255];
 	char dest_name[255];
+  char dir[255] = "img/";
 
 	strcpy(src_name, argv[1]);
 	strcpy(filter_t, argv[2]);
@@ -51,13 +82,13 @@ int main(int argc, char* argv[]) {
 
 	cudaMemcpy(dev_src, src->imageData, size, cudaMemcpyHostToDevice);
 
- 
-    gray<<<src->height, src->width>>>(dev_src, dev_dest, src->width, src->height, step, src->nChannels);
- 
+
+  gray<<<src->height, src->width>>>(dev_src, dev_dest, src->width, src->height, step, src->nChannels);
+
 
 	cudaMemcpy(dest->imageData, dev_dest, size, cudaMemcpyDeviceToHost);
 
-	cvSaveImage("test.png" , dest);
+	cvSaveImage(strcat(dir,dest_name) , dest);
 
 	cudaFree(dev_dest);
 	cudaFree(dev_src);
